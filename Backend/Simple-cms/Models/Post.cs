ff1d766724be9f284659121490a1c1f9ba822155
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-10-16 17:39:24 +0200

    Initial setup of the backend. Added Post model

---
+++ Backend/Simple-cms/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Simple_cms.Models
+{
+    public class Post
+    {
+        public int PostId { get; }
+        public string Title { get; }
+        public string Preamble { get; }
+        public string BodyText { get; }
+        public string PostCategory { get; }
+        public string ImageThumbnail { get; }
+
+    }
+}

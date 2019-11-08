2ce7b969fc2729a526f2a131523d748e4031eaa3
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-11-08 17:27:51 +0100

    Can now change name of a Page

--- Frontend/simple-cms/src/Pages/Admin/PagesAdmin/EditPagePage.js
+++ Frontend/simple-cms/src/Pages/Admin/PagesAdmin/EditPagePage.js
@@ -11,12 +11,13 @@ class EditPagePage extends Component {
             page: [],
             pageId: "",
             pageName: "",
+            changePageName: false,
         }
     }
 
     componentDidMount() {
         const { id } = this.props.match.params;
-        this.getPageByKey();
+        this.getPageByKey(id);
         this.setState({
             pageId: id,
         })
@@ -34,35 +35,91 @@ class EditPagePage extends Component {
                     'Content-Type': 'application/json',
                 },
             })
+                .then(() => {
+                    window.location.reload();
+                })
         }
     }
 
-    getPageByKey = () => {
-        const api = `http://localhost:5000/api/page/${this.props.match.params.id}`;
+    UpdatePageName = (id) => {
+        const api = `http://localhost:5000/api/page/${id}`;
+
+        fetch(api, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                page_name: this.state.pageName,
+            })
+        })
+        .then(() => {
+            window.location.reload();
+        })
+
+    }
+
+    getPageByKey = (id) => {
+        const api = `http://localhost:5000/api/page/${id}`;
 
         fetch(api)
             .then(res => res.json())
             .then(item => {
-                this.setState({
-                    page: item
-                });
-            });
+                this.setState({ page: item });
+            })
+    }
+
+    changePageNameCheck = () => {
+        this.setState(prevState => ({
+            changePageName: !prevState.changePageName,
+        }))
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.UpdatePageName(this.state.pageId);
+    }
+
+    changeHandler = e => {
+        this.setState({ [e.target.name]: e.target.value })
     }
 
     render() {
-        const { page } = this.state
-        console.log(page);
-        
+        const { page, changePageName, pageName } = this.state
+
         return (
             <Fragment>
                 <AdminNavbar />
                 <h1>Edit Page</h1>
                 {
                     page.map((page, key) =>
-                    <div className="page" key={key}>
-                            <h4>{page.page_name} </h4>
-                    <h4>Add new post to this page</h4>
-                    <button><Link to={`/admin/posts/add-post/`}>Add new post</Link></button>
+                        <div className="page" key={key}>
+                            {
+                                changePageName === true
+
+                                    ? <Fragment>
+                                        <form onSubmit={this.handleSubmit}>
+                                            <label>
+                                                Enter new page name:
+                                                    <input
+                                                    type="text"
+                                                    name="pageName"
+                                                    value={pageName}
+                                                    onChange={this.changeHandler}
+                                                />
+                                            </label>
+                                            <input type="submit" value="Submit" />
+                                        </form>
+                                    </Fragment>
+
+                                    : <Fragment>
+                                        <h4>{page.page_name} </h4>
+                                        <button onClick={() => this.changePageNameCheck()}>Change name of this page</button>
+                                    </Fragment>
+                            }
+                            <h4>Add new post to this page</h4>
+                            <button><Link to={`/admin/posts/add-post/`}>Add new post</Link></button>
                             {
                                 page.posts.map((post, postKey) =>
                                     post === null
@@ -74,8 +131,7 @@ class EditPagePage extends Component {
                                             <p>{post.body_text}</p>
                                             <div className="page-action-list">
                                                 <button><Link to={`/admin/posts/edit-post/${post.post_id}`}>Edit post</Link></button>
-                                                <button onClick={() => this.deletePost(page.page_id)}>Delete post</button>
-
+                                                <button onClick={() => this.deletePost(post.post_id)}>Delete post</button>
                                             </div>
                                         </div>
                                 )
